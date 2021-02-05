b1fd84c02117df1059beff6ade3d203924bbca90
Jake Stewart <jake.stewart.us@gmail.com>
2021-02-05 02:26:17 -0800

    Created Firestore Config
    Application connects to Firestore database and can add data to it.

---
+++ DataLibrary/DataAccess/FirestoreConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+namespace DataLibrary.DataAccess
+{
+    public class FirestoreConfig
+    {
+        // Recommended to keep one instance of FirestoreDb
+        public static FirestoreDb db;
+
+        // Connect to Google Cloud Firestore
+        public static void InitFirestore()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"serviceAccountKey.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            db = FirestoreDb.Create("tracker-10886");
+            //CollectionReference collection = db.Collection("Tickets");
+            //Dictionary<string, object> data1 = new Dictionary<string, object>()
+            //{
+            //    {"number", "five"}
+            //};
+            //collection.AddAsync(data1);
+        }
+    }
+}
--- IssueTracker/Global.asax.cs
+++ IssueTracker/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLibrary.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace IssueTracker
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            FirestoreConfig.InitFirestore();
         }
     }
 }
